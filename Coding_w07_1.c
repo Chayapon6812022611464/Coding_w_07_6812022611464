33cddecc7717653c053718bbf6bd6d6bc58ea71e
Chayapon6812022611464 <s6812022611464@email.kmutnb.ac.th>
2025-09-02 15:31:35 +0700

    Add files via upload

---
+++ Coding_w07_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() { 
+
+    float score; //ประกาศตัวแปร score
+    float score_plus; //ประกาศตัวแปร score_plus เพื่อใช้ในการคำนวณ
+
+    printf("Please enter your score : "); 
+    scanf("%f", &score); //แสดงข้อความให้ผู้ใช้ใส่ค่า คะแนน
+    
+    if (score >= 50) {
+        score_plus = score + (score*0.05); //ถ้าคะแนนมากกว่าหรือเท่ากับ 50 ให้นำคะแนนไปคำนวณบวกคะแนนเพิ่มไปอีก 5%
+    } else {
+        score_plus = score; //ถ้าคะแนนน้อยกว่า 50 จะไม่ได้การบวกคะแนนเพิ่ม
+    }
+
+    printf("Your final score is : %.2f\n", score_plus); //แสดงผลคะแนนหลังจากการคำนวณแล้ว
+    printf("End of evaluation\n"); //แสดงข้อความจบการคำนวณ
+
+    return 0;
+
+}
